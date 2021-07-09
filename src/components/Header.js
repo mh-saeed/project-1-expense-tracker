3f24e0f5312400df5a1be098491d8767c699693b
mh-saeed <saeedkhan111244@gmail.com>
2021-07-09 22:44:48 +0500

    final changes added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,13 @@ export const Header = () => {
   return (
     <div className="header">
       <h2>Expense Tracker</h2>
+      <p>
+        created by
+        <a href="https://github.com/mh-saeed/project-1-expense-tracker">
+          {" "}
+          mh-saeed
+        </a>
+      </p>
     </div>
   );
 };
