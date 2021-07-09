9f9dca0e250926f4be862b97b10f673eaf5bce1f
mh-saeed <saeedkhan111244@gmail.com>
2021-07-09 22:16:54 +0500

    updated

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,13 +4,13 @@ import AppReducer from "./AppReducer";
 // initial state
 const initialState = {
   transactions: [
-    { id: 1, text: `ice-cream`, amount: -500 },
-    { id: 2, text: `Cake`, amount: 100 },
-    { id: 3, text: `cream`, amount: 400 },
-    { id: 4, text: `chocolate`, amount: -200 },
-    { id: 5, text: `lunch`, amount: -300 },
-    { id: 6, text: `salary`, amount: 300 },
-    { id: 7, text: `tuitions-income`, amount: 300 },
+    { id: 1, text: `Salary`, amount: 1500 },
+    { id: 2, text: `electricity-bill`, amount: -200 },
+    { id: 3, text: `utility-stuff`, amount: -400 },
+    { id: 4, text: `monthly-bonus`, amount: 500 },
+    { id: 5, text: `school-fee`, amount: -150 },
+    { id: 6, text: `crypto-income`, amount: 2300 },
+    { id: 7, text: `tax-paid`, amount: -250 },
   ],
 };
 
